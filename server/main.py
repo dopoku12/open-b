1d0a5526acb11b8c9b96d7d77cada78f10e4ee87
dopoku12 <dopoku.tech@gmail.com>
2024-12-01 17:57:22 -0500

    prod changes

--- server/main.py
+++ server/main.py
@@ -1,15 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-import io
-import os
 from dotenv import load_dotenv
 
 from Apis import censusApi
 
 load_dotenv()
 
-#missing districts
-#nsa='https://geodata.baltimorecity.gov/egis/rest/services/CityView/Neighborhoods/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
 app = Flask(__name__)
 CORS(app)
 
@@ -28,17 +24,6 @@ def  home():
         url=item['url']
         data= list(map(lambda data:data['properties'],censusApi.make_req(url)['features']))
         return data
-                
-# @app.route('/input', methods=['POST'])
-# def userInput():
-#     print("Request:",request)
-#     # Check if text part exists
-#     input_text = request.form.get('text')
-#     if input_text:
-#         print(input_text)
-
-#     else:
-#         return jsonify({'error': 'No text part'}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
