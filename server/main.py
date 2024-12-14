4884a2ad014efa39f8b18a8af122a9b81d1728b2
dopoku12 <dopoku.tech@gmail.com>
2024-12-13 22:10:12 -0500

    deleted Api

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 import requests
@@ -81,4 +81,4 @@ def home():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(host="0.0.0.0", port=80, debug=False)
